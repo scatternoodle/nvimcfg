a577150fcb3c3ffa2689dd146ab22f63218a6479
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-08-22 21:16:57 +0100

    update config

--- init.lua
+++ init.lua
@@ -801,23 +801,6 @@ require("lazy").setup({
 		priority = 1000,
 	},
 
-	-- { -- You can easily change to a different colorscheme.
-	-- Change the name of the colorscheme plugin below, and then
-	-- change the command in the config to whatever the name of that colorscheme is.
-	--
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	-- priority = 1000, -- Make sure to load this before all the other start plugins.
-	-- init = function()
-	-- Load the colorscheme here.
-	-- Like many other themes, this one has different styles, and you could load
-	-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-	-- vim.cmd.colorscheme("rosebones")
-
-	-- You can configure highlights by doing something like:
-	-- vim.cmd.hi("Comment gui=none")
-	-- end,
-	-- },
-
 	-- Highlight todo, notes, etc in comments
 	{
 		"folke/todo-comments.nvim",
@@ -847,17 +830,17 @@ require("lazy").setup({
 			-- Simple and easy statusline.
 			--  You could remove this setup call if you don't like it,
 			--  and try some other statusline plugin
-			local statusline = require("mini.statusline")
+			-- local statusline = require("mini.statusline")
 			-- set use_icons to true if you have a Nerd Font
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
+			-- statusline.setup({ use_icons = vim.g.have_nerd_font })
 
 			-- You can configure sections in the statusline by overriding their
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
+			--@diagnostic disable-next-line: duplicate-set-field
+			--statusline.section_location = function()
+			--return "%2l:%-2v"
+			--end
 
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
@@ -955,7 +938,7 @@ require("lazy").setup({
 })
 
 -- setcolorscheme actual (after all colorscheme plugins have been loaded)
-vim.cmd.colorscheme("duckbones")
+vim.cmd.colorscheme("rosebones")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/ext/plugins/lualine.lua
+++ lua/ext/plugins/lualine.lua
@@ -4,4 +4,8 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		require("lualine").setup()
+	end,
 }
