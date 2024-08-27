075890b6cb82cae69d53e7d2aa221f1e61980977
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-08-27 08:22:30 +0100

    config updates

--- init.lua
+++ init.lua
@@ -860,7 +860,7 @@ require("lazy").setup({
 })
 
 -- setcolorscheme actual (after all colorscheme plugins have been loaded)
-vim.cmd.colorscheme("solarized-osaka")
+vim.cmd.colorscheme("rosebones")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
