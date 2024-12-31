394ef207e0e724fb95679cf998f78134be100b83
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2024-12-31 15:44:00 +0000

    add .gs extension (Google Apps Script) to javascript filetype

--- init.lua
+++ init.lua
@@ -181,6 +181,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- [[ Filetypes ]]
+-- See `:help vim.filetype.add`
+vim.filetype.add({
+	extension = {
+		gs = "javascript",
+	},
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
